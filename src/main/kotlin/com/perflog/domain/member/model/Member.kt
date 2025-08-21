5bc3e7e2739b07ea64a6876a59851357bebabfd2
Heeyeon An <gmldus4613@naver.com>
2025-08-21 18:52:48 +0900

    feat(member): 연관관계 개선 (Review 양방향, RefreshToken 일대일) #26

--- src/main/kotlin/com/perflog/domain/member/model/Member.kt
+++ src/main/kotlin/com/perflog/domain/member/model/Member.kt
@@ -1,5 +1,8 @@
 package com.perflog.domain.member.model
 
+import com.fasterxml.jackson.annotation.JsonManagedReference
+import com.perflog.common.model.BaseTimeEntity
+import com.perflog.domain.review.model.Review
 import jakarta.persistence.*
 
 @Entity
@@ -18,8 +21,12 @@ class Member(
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
     var role: MemberRole
-) {
+) : BaseTimeEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0
+
+    @JsonManagedReference("member-reviews")
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+    val reviews: MutableList<Review> = mutableListOf()
 }
